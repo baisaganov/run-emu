cd3da5814d57b7c55b4a9dfc515e7b55bb717503
Alisher Baisaganov <baisaganov99@gmail.com>
2025-01-11 17:59:13 +0500

    test

--- main.py
+++ main.py
@@ -2,17 +2,34 @@ import random
 import subprocess
 import time
 
+
+emulator_path = r"C:\Program Files (x86)\Nox\bin\Nox.exe"
+
 # Список эмуляторов и времени их запуска (в секундах)
 emulators = [
-    {"name": "Emulator1", "path": "C:\\Users\\o7\\Desktop\\emu\\arimka.lnk", "interval": 4},  # 4 часа
-    # {"name": "Emulator2", "path": "path/to/emulator2.exe", "interval": 6},  # 5 часов
-    # Добавьте остальные эмуляторы
+    {"name": "Emulator1", "path": "-clone:Nox_9", "interval": 4},  # 4 часа
+    {"name": "Emulator1", "path": "-clone:Nox_1", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_2", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_11", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_3", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_4", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_10", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_5", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_6", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_7", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "-clone:Nox_8", "interval": 4},  # 5 часов
+    {"name": "Emulator1", "path": "", "interval": 4}  # 5 часов
 ]
 
 # Функция для запуска эмулятора
 def run_emulator(emulator):
     print(f"Запускается: {emulator['name']}")
-    subprocess.Popen(emulator['path'])  # Открытие эмулятора
+    args = [emulator['path']]
+    try:
+        subprocess.Popen([emulator_path, *args], shell=True)
+        print("Эмулятор запущен с параметрами:", " ".join(args))
+    except Exception as e:
+        print(f"Ошибка при запуске эмулятора: {e}")
     time.sleep(120)  # Задержка для корректной инициализации
 
 # Основной цикл
