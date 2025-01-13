5e08c83d27e60f0b6bd6df771e19c9ad47b225e5
Alisher Baisaganov <baisaganov99@gmail.com>
2025-01-13 16:14:49 +0500

    доработка таймингов

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ nox_path = "C:\\Program Files (x86)\\Nox\\bin\\Nox.exe"
 # Список эмуляторов и времени их запуска (в секундах)
 emulators = [
     {"name": "ari", "path": "-clone:Nox_9", "interval": 24},  # 24 часа
-    {"name": "david", "path": "-clone:Nox_1", "interval": 6},  # 5 часов
-    {"name": "hard", "path": "-clone:Nox_2", "interval": 6},  # 5 часов
+    {"name": "david", "path": "-clone:Nox_1", "interval": 12},  # 5 часов
+    {"name": "hard", "path": "-clone:Nox_2", "interval": 12},  # 5 часов
     {"name": "join", "path": "-clone:Nox_11", "interval": 24},  # 5 часов
-    {"name": "lisan", "path": "-clone:Nox_3", "interval": 6},  # 5 часов
+    {"name": "lisan", "path": "-clone:Nox_3", "interval": 24},  # 5 часов
     {"name": "lux", "path": "-clone:Nox_4", "interval": 12},  # 5 часов
-    {"name": "mistie", "path": "-clone:Nox_10", "interval": 12},  # 5 часов
-    {"name": "revor_", "path": "-clone:Nox_5", "interval": 4},  # 5 часов
+    {"name": "mistie", "path": "-clone:Nox_10", "interval": 24},  # 5 часов
+    {"name": "revor_", "path": "-clone:Nox_5", "interval": 6},  # 5 часов
     {"name": "mo0n", "path": "-clone:Nox_6", "interval": 24},  # 5 часов
-    {"name": "ministr", "path": "-clone:Nox_7", "interval": 4},  # 5 часов
-    {"name": "alpaka", "path": "-clone:Nox_8", "interval": 4},  # 5 часов
-    {"name": "compart", "path": "", "interval": 6}  # 5 часов
+    {"name": "ministr", "path": "-clone:Nox_7", "interval": 6},  # 5 часов
+    {"name": "alpaka", "path": "-clone:Nox_8", "interval": 6},  # 5 часов
+    {"name": "compart", "path": "", "interval": 12}  # 5 часов
 ]
 
 # Функция для проверки, запущен ли процесс Nox.exe
@@ -57,13 +57,13 @@ def run_emulator(emulator):
         # Ожидание указанного интервала
         print(f"[{datetime.now()}] Ожидание {emulator['interval']} секунд до следующего запуска {emulator['name']}")
         if emulator['interval'] == 4:
-            time.sleep(random.randint(13500, 15500))
+            time.sleep(random.randint(int(3600*4-3600*4*0.2), int(3600*4+3600*4*0.2)))
         elif emulator['interval'] == 6:
-            time.sleep(random.randint(20000, 23000))
+            time.sleep(random.randint(int(3600*6-3600*6*0.2), int(3600*6+3600*6*0.2)))
         elif emulator['interval'] == 12:
-            time.sleep(random.randint(42000, 45600))
+            time.sleep(random.randint(int(3600*12-3600*12*0.2), int(3600*12+3600*12*0.2)))
         elif emulator['interval'] == 24:
-            time.sleep(random.randint(84000, 91200))
+            time.sleep(random.randint(int(3600*24-3600*24*0.2), int(3600*24+3600*24*0.2)))
 
 
 # Создание потоков для каждого эмулятора
